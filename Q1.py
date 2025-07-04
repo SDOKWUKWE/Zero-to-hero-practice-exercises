dbb4f57bab1f86da558d49142338b5f858a8a267
SDOKWUKWE <kingsamoextra@gmail.com>
2025-07-04 13:56:37 +0100

    my first commit

---
+++ Q1.py
@@ -0,0 +1,15 @@
+"""
+find the area of a circle
+where r = 1.1
+formula for area = pi **r
+"""
+'''x=0
+def func():
+    x += 1
+    print(x)
+'''
+
+x=(5+15/3)**2/50
+print(x)
+
+print("i love python")
